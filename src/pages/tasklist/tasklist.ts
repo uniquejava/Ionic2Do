9cfb3318c95f3766682517ca1efd9665b571e1c3
cyper <345343747@qq.com>
2017-03-17 19:46:43 +0800

    save to firebase and use native prompt dialog

--- src/pages/tasklist/tasklist.ts
+++ src/pages/tasklist/tasklist.ts
@@ -1,39 +1,46 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 import {NavController, ItemSliding} from 'ionic-angular';
 import {Task} from "./task";
-
+import {AngularFire, FirebaseListObservable} from 'angularfire2'
+import {Dialogs} from "ionic-native";
 @Component({
   selector: 'page-tasklist',
   templateUrl: 'tasklist.html'
 })
 export class TaskListPage {
-  tasks: Array<Task> = [];
-  constructor(public navCtrl: NavController) {
-    this.tasks = [
-      {title: 'Milk', status: 'open'},
-      {title: 'Eggs', status: 'open'},
-      {title: 'Syrup', status: 'open'},
-      {title: 'Pancake Mix', status: 'open'}
-    ];
-  }
-  addItem(){
-    let theNewTask: string = prompt("New Task");
-    if (theNewTask !== '') {
-      this.tasks.push({title: theNewTask, status: 'open'});
-    }
+  tasks: FirebaseListObservable<any[]>;
+
+  constructor(public navCtrl: NavController, public af: AngularFire) {
+    this.tasks = af.database.list('/tasks');
   }
 
-  markAsDone(slidingItem: ItemSliding, task: Task){
-    task.status = 'done';
+  addItem() {
+    // let theNewTask: string = prompt("New Task");
+    // if (theNewTask !== '') {
+    //   this.tasks.push({title: theNewTask, status: 'open'});
+    // }
+    Dialogs.prompt('Add a task', "Ionic2Do", ['Ok', 'Cancel'], '')
+      .then(theResult => {
+        if ((theResult.buttonIndex == 1) && (theResult.input1 !== '')) {
+          this.tasks.push({title: theResult.input1, status: 'open'});
+        }
+      });
+  }
+
+  markAsDone(slidingItem: ItemSliding, task: Task) {
+    // task.status = 'done';
+    this.tasks.update(task.$key, {status: 'done'});
     slidingItem.close();
   }
+
   removeTask(slidingItem: ItemSliding, task: Task) {
-    task.status = 'removed';
-    let index = this.tasks.indexOf(task);
-    if (index > -1) {
-      this.tasks.splice(index, 1);
-    }
+    // task.status = 'removed';
+    // let index = this.tasks.indexOf(task);
+    // if (index > -1) {
+    //   this.tasks.splice(index, 1);
+    // }
+    this.tasks.remove(task.$key);
     slidingItem.close();
   }
 
